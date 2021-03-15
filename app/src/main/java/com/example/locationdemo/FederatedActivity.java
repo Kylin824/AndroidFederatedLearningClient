40e71df39e2418a7c0f2cbbb59900790859e430d
CalvinLin <31285778+Calvin24lky@users.noreply.github.com>
2021-03-15 22:13:02 +0800

    add client procedure

--- app/src/main/java/com/example/locationdemo/FederatedActivity.java
+++ app/src/main/java/com/example/locationdemo/FederatedActivity.java
@@ -7,7 +7,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -23,6 +22,7 @@ import org.json.JSONObject;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import io.socket.client.IO;
 import io.socket.client.Socket;
@@ -38,11 +38,13 @@ public class FederatedActivity extends AppCompatActivity {
     private String mUsername;
     private Boolean isConnected = true;
     Handler handler = null;
+    private String clientID;
 
     private Socket mSocket;
+
     {
         try {
-            mSocket = IO.socket("http://10.0.2.2:8088");  // 模拟器用10.0.2.2对应server的localhost
+            mSocket = IO.socket("http://10.0.2.2:9092");  // 模拟器要连接里面的wifi！用10.0.2.2对应server的localhost
         } catch (URISyntaxException e) {
             Log.e(TAG, "onCreate: " + e.toString());
         }
@@ -77,26 +79,30 @@ public class FederatedActivity extends AppCompatActivity {
         preProcessBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mUsername = "user" + Math.floor((Math.random() * 1000) + 1);
+                mUsername = "android_" + Math.floor((Math.random() * 1000) + 1);
                 mSocket.on(Socket.EVENT_CONNECT, onConnect);
+                mSocket.on(Socket.EVENT_DISCONNECT, onDisConnect);
                 mSocket.on(Socket.EVENT_CONNECT_ERROR, onConnectError);
-                mSocket.on("new message", onNewMessage);
+                mSocket.on("chatevent", onNewMessage);
+                mSocket.on("init", onInit);
+                mSocket.on("request_update", onRequestUpdate);
+                mSocket.on("stop_and_eval", onStopAndEval);
                 mSocket.connect();
             }
         });
 
-
         startManualBtn.setOnClickListener(v -> {
-            step++;
-            Log.d(TAG, "onClick: step: " + step);
-            addEntry(step);
+//            step++;
+//            Log.d(TAG, "onClick: step: " + step);
+//            addEntry(step);
+            mSocket.emit("client_wake_up");
+            logArea.append("send wake up \n");
         });
 
         startAutoBtn.setOnClickListener(v -> {
-
             JSONObject data = new JSONObject();
             try {
-                data.put("username", "android_0");
+                data.put("userName", "android_0");
                 data.put("message", "this is a msg from android");
             } catch (JSONException e) {
                 Log.e(TAG, "onCreate: " + e.toString());
@@ -112,58 +118,67 @@ public class FederatedActivity extends AppCompatActivity {
             return false;
         });
 
-
     }
 
     private void addEntry(int step) {
         LineData lineData = mChart.getData();
         float ydata = (float) (Math.random() * 20);
         lineData.addEntry(new Entry(step, ydata), 0);
-
         mChart.setVisibleXRangeMaximum(step + 5);
-
         mChart.notifyDataSetChanged();
         mChart.invalidate();
-
         mChart.animateX(500);
     }
 
-//    private Emitter.Listener onNewMessage = new Emitter.Listener() {
-//        @Override
-//        public void call(final Object... args) {
-//            JSONObject data = (JSONObject) args[0];
-//            String username;
-//            String message;
-//            try {
-//                username = data.getString("username");
-//                message = data.getString("message");
-//                Log.d(TAG, "onCreate: received from server: " + message);
-//                Message msg = new Message();
-//                Bundle bundle = new Bundle();
-//                bundle.putString("data", message);
-//                msg.setData(bundle);
-//                handler.sendMessage(msg);
-//            } catch (JSONException e) {
-//                Log.e(TAG, e.toString());
-//            }
-//        }
-//    };
+    private Emitter.Listener onConnect = new Emitter.Listener() {
+        @Override
+        public void call(Object... args) {
+            Message msg = new Message();
+            Bundle bundle = new Bundle();
+            bundle.putString("data", "connect to server");
+            msg.setData(bundle);
+            handler.sendMessage(msg);
+        }
+    };
+
+    private Emitter.Listener onConnectError = new Emitter.Listener() {
+        @Override
+        public void call(Object... args) {
+            Exception err = (Exception) args[0];
+            Message msg = new Message();
+            Bundle bundle = new Bundle();
+            bundle.putString("data", "connect error");
+            msg.setData(bundle);
+            handler.sendMessage(msg);
+            Log.d(TAG, "onCreate: connect error" + err.getMessage());
+            // err.printStackTrace();
+        }
+    };
+
+    private Emitter.Listener onDisConnect = new Emitter.Listener() {
+        @Override
+        public void call(Object... args) {
+            Message msg = new Message();
+            Bundle bundle = new Bundle();
+            bundle.putString("data", "disconnect to server");
+            msg.setData(bundle);
+            handler.sendMessage(msg);
+        }
+    };
 
     private Emitter.Listener onNewMessage = new Emitter.Listener() {
         @Override
         public void call(final Object... args) {
             JSONObject data = (JSONObject) args[0];
-            String clientId;
-            String clientSocketId;
-            String clientGradient;
-            String extraMsg;
             try {
-                username = data.getString("username");
-                message = data.getString("message");
-                Log.d(TAG, "onCreate: received from server: " + message);
+                String username = data.getString("userName");
+                String message = data.getString("message");
+
+                String msgFromServer = "user_" + username + " : " + message;
+                Log.d(TAG, "onCreate: received from server: " + msgFromServer);
                 Message msg = new Message();
                 Bundle bundle = new Bundle();
-                bundle.putString("data", message);
+                bundle.putString("data", msgFromServer);
                 msg.setData(bundle);
                 handler.sendMessage(msg);
             } catch (JSONException e) {
@@ -172,68 +187,94 @@ public class FederatedActivity extends AppCompatActivity {
         }
     };
 
-//    private Emitter.Listener onConnect = new Emitter.Listener() {
-//        @Override
-//        public void call(Object... args) {
-//            runOnUiThread(new Runnable() {
-//                @Override
-//                public void run() {
-//                    if (!isConnected) {
-//                        if (null != mUsername)
-//                            mSocket.emit("add user", mUsername);
-//                        Toast.makeText(FederatedActivity.this, "connect success", Toast.LENGTH_SHORT).show();
-//                        isConnected = true;
-//                    }
-//                }
-//            });
-//        }
-//    };
-    private Emitter.Listener onConnect = new Emitter.Listener() {
+    private Emitter.Listener onInit = new Emitter.Listener() {
         @Override
-        public void call(Object... args) {
-            if (!isConnected) {
-                if (null != mUsername)
-                    mSocket.emit("add user", mUsername);
+        public void call(final Object... args) {
+            JSONObject data = (JSONObject) args[0];
+            try {
+                String initWeights = data.getString("initWeights");
+                Integer epoch = data.getInt("epoch");
+                Integer batchSize = data.getInt("batchSize");
+                Integer clientIndex = data.getInt("clientIndex");
+
+                // init local model by init weights
+
+                String msgFromServer = "init_weight: " + initWeights + " \n epoch: " + epoch;
+
                 Message msg = new Message();
                 Bundle bundle = new Bundle();
-                bundle.putString("data", "connect success");
+                bundle.putString("data", msgFromServer);
                 msg.setData(bundle);
                 handler.sendMessage(msg);
-                isConnected = true;
+
+                Integer trainSize = 60000;
+
+                JSONObject resp = new JSONObject();
+                try {
+                    resp.put("trainSize", trainSize);
+                } catch (JSONException e) {
+                    Log.e(TAG, "onInit: " + e.getMessage());
+                }
+                mSocket.emit("client_ready", resp);
+            } catch (JSONException e) {
+                Log.e(TAG, e.toString());
             }
         }
     };
 
 
-//    private Emitter.Listener onConnectError = new Emitter.Listener() {
-//        @Override
-//        public void call(Object... args) {
-//            runOnUiThread(new Runnable() {
-//                @Override
-//                public void run() {
-//                    Log.e(TAG, "Error connecting");
-//                    Toast.makeText(FederatedActivity.this, "connect error", Toast.LENGTH_SHORT).show();
-//                }
-//            });
-//        }
-//    };
-
-    private Emitter.Listener onConnectError = new Emitter.Listener() {
+    private Emitter.Listener onRequestUpdate = new Emitter.Listener() {
         @Override
-        public void call(Object... args) {
-            Message msg = new Message();
-            Bundle bundle = new Bundle();
-            bundle.putString("data", "connect error");
-            msg.setData(bundle);
-            handler.sendMessage(msg);
-            Log.d(TAG, "onCreate: connect error");
+        public void call(final Object... args) {
+            JSONObject data = (JSONObject) args[0];
+            try {
+                String modelId = data.getString("modelId");
+                String weights = data.getString("weights");
+                Integer currentRound = data.getInt("currentRound");
+
+                // set global weight to local model weight
+
+                // train one round to get new weights
+
+                String newWeights = "weight_client_round_" + currentRound;
+
+                JSONObject resp = new JSONObject();
+                try {
+                    resp.put("roundNumber", currentRound);
+                    resp.put("weights", newWeights);
+                } catch (JSONException e) {
+                    Log.e(TAG, "onRequestUpdate: " + e.getMessage());
+                }
+                mSocket.emit("client_update", resp);
+            } catch (JSONException e) {
+                Log.e(TAG, e.toString());
+            }
+        }
+    };
+
+    private Emitter.Listener onStopAndEval = new Emitter.Listener() {
+        @Override
+        public void call(final Object... args) {
+            JSONObject req = (JSONObject) args[0];
+            try {
+                String finalGlobalAcc = req.getString("finalGlobalTestAcc");
+
+                Message msg = new Message();
+                Bundle bundle = new Bundle();
+                bundle.putString("data", "final global acc: " + finalGlobalAcc);
+                msg.setData(bundle);
+                handler.sendMessage(msg);
+
+            } catch (JSONException e) {
+                Log.e(TAG, e.toString());
+            }
         }
     };
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        mSocket.off("new message", onNewMessage);
+        mSocket.off("chatevent", onNewMessage);
         mSocket.disconnect();
     }
 
